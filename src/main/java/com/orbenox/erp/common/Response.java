a3dba46a1e60e5e46c230bf8bf06aac84fc86602
museyib <museyib.e@gmail.com>
2026-01-18 23:11:18 +0400

    Response - success

--- src/main/java/com/orbenox/erp/common/Response.java
+++ src/main/java/com/orbenox/erp/common/Response.java
@@ -9,28 +9,29 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Response<T> {
+    private boolean success;
     private int code;
     private String message;
     private String messageKey;
     private T data;
 
     public static <T> Response<T> successData(T data) {
-        return new Response<>(HttpStatus.OK.value(), "success", "", data);
+        return new Response<>(true, HttpStatus.OK.value(), "success", "", data);
     }
 
     public static <T> Response<T> successMessage(String message, String messageKey) {
-        return new Response<>(HttpStatus.OK.value(), message, messageKey, null);
+        return new Response<>(true, HttpStatus.OK.value(), message, messageKey, null);
     }
 
     public static <T> Response<T> errorData(int code, String message, String messageKey, T data) {
-        return new Response<>(code, message, messageKey, data);
+        return new Response<>(false, code, message, messageKey, data);
     }
 
     public static <T> Response<T> errorMessage(int code, String message) {
-        return new Response<>(code, message, "", null);
+        return new Response<>(false, code, message, "", null);
     }
 
     public static <T> Response<T> errorMessage(int code, String message, String messageKey) {
-        return new Response<>(code, message, messageKey, null);
+        return new Response<>(false, code, message, messageKey, null);
     }
 }
